f9899be2759975ded5b7d995503c7af94a875b32
d-smith <doug.smith@fmr.com>
2016-02-22 14:32:07 -0800

    Updated env to align with seeded app

--- setenv.sh
+++ setenv.sh
@@ -4,8 +4,8 @@
 # callback sample. The sample assumes it's an application that
 # was registered with the application roll.
 
-export AZ_SERVER=MACLB015803:3000
-export CLIENT_ID=d80c9758-f965-4286-7003-e5af065d8082
-export CLIENT_SECRET=4suKio+75Vsm2o+i8H8dEjxVbXhoB81G+rJolRw8+XU=
+export AZ_SERVER=localhost:3000
+export CLIENT_ID=5d130f17-2fe5-4462-4e9d-9b6eb2d806e8
+export CLIENT_SECRET=0KizQCINnU0DtIkgwGs5ipc1AMt3WfUU1lNt6zTQTu4=
 export REDIRECT_URI=http://localhost:2000/oauth2_callback
 export ECHO_ENDPOINT=http://localhost:5000
